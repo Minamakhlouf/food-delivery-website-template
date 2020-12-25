c154dacdf8e11b204fbdc3543129b9aa63420809
Mina Makhlouf <mina.makhlouf@gmail.com>
2020-12-25 14:27:12 -0500

    media queries and responsive nav

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const burger = document.querySelector(".burger"); 
 const links = document.querySelector("header ul"); 
+const navLinks = document.querySelectorAll("header ul li"); 
+console.log(navLinks)
 
 burger.addEventListener("click", () => {
     links.classList.toggle("nav-active"); 
+    navLinks.forEach((link, index) => {
+        if(link.style.animation) {
+            link.style.animation = ""; 
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+        }
+    });
+    burger.classList.toggle("clicked"); 
 }); 
